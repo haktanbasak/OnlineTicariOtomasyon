0a8d822a68695166d9f7d22ca55d5b3de2db1ab9
Haktan Başak <haktanbasak9@gmail.com>
2022-08-10 12:20:48 +0300

    Cari İşlemleri

---
+++ OnlineTicariOtomasyon/Controllers/CariController.cs
@@ -0,0 +1,65 @@
+﻿using OnlineTicariOtomasyon.Models.Sınıflar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineTicariOtomasyon.Controllers
+{
+    public class CariController : BaseController
+    {
+        // GET: Cari
+        public ActionResult Index()
+        {
+            var model = db.Caris.Where(x=>x.Durum==true).ToList();
+            return View(model);
+        }
+
+        [HttpGet]
+        public ActionResult CariEkle() {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CariEkle(Cari cari)
+        {
+            db.Caris.Add(cari);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult CariSil(int id) {
+            var cari = db.Caris.Find(id);
+            cari.Durum = false;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult CariGetir(int id) {
+            var model = db.Caris.Find(id);
+            return View("CariGetir", model);
+        }
+
+        public ActionResult CariGuncelle(Cari cari)
+        {
+            if (!ModelState.IsValid) {
+                return View("CariGetir");
+            }
+            var car = db.Caris.Find(cari.CariID);
+            car.CariAd = cari.CariAd;
+            car.CariSoyad = cari.CariSoyad;
+            car.CariSehir = cari.CariSehir;
+            car.CariMail = cari.CariMail;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult MusteriSatis(int id) {            
+            var deger = db.SatisHarekets.Where(x => x.CariId == id).ToList();
+            var cari = db.Caris.Where(x => x.CariID == id).Select(y => y.CariAd + " " + y.CariSoyad).FirstOrDefault();
+            ViewBag.car = cari;
+            return View(deger);
+        }
+    }
+}
--- OnlineTicariOtomasyon/Models/Sınıflar/Cari.cs
+++ OnlineTicariOtomasyon/Models/Sınıflar/Cari.cs
@@ -10,11 +10,12 @@ namespace OnlineTicariOtomasyon.Models.Sınıflar
         public int CariID { get; set; }
 
         [Column(TypeName = "Varchar")]
-        [StringLength(30)]
+        [StringLength(30, ErrorMessage ="En fazla 30 karakter girebilirsiniz")]
         public string CariAd { get; set; }
 
         [Column(TypeName = "Varchar")]
         [StringLength(30)]
+        [Required(ErrorMessage ="Bu alanı boş geçemezsiniz!")]
         public string CariSoyad { get; set; }
 
         [Column(TypeName = "Varchar")]
@@ -24,6 +25,7 @@ namespace OnlineTicariOtomasyon.Models.Sınıflar
         [Column(TypeName = "Varchar")]
         [StringLength(50)]
         public string CariMail { get; set; }
+        public bool Durum { get; set; }
         public ICollection<SatisHareket> SatisHarekets { get; set; }
     }
 }
