b0e6d9159ab245d4fdfabcdc33860a5ec5bdf02a
Haktan Başak <haktanbasak9@gmail.com>
2022-08-12 16:28:00 +0300

    Fatura listeleme ve ekleme

---
+++ OnlineTicariOtomasyon/Controllers/FaturaController.cs
@@ -0,0 +1,34 @@
+﻿using OnlineTicariOtomasyon.Models.Sınıflar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineTicariOtomasyon.Controllers
+{
+    public class FaturaController : BaseController
+    {
+        // GET: Fatura
+        public ActionResult Index()
+        {
+            var model = db.Faturas.ToList();
+            return View(model);
+        }
+
+        [HttpGet]
+        public ActionResult FaturaEkle()
+        {            
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult FaturaEkle(Fatura fatura)
+        {
+            db.Faturas.Add(fatura);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
--- OnlineTicariOtomasyon/Models/Sınıflar/Fatura.cs
+++ OnlineTicariOtomasyon/Models/Sınıflar/Fatura.cs
@@ -23,7 +23,10 @@ namespace OnlineTicariOtomasyon.Models.Sınıflar
         [Column(TypeName = "Varchar")]
         [StringLength(60)]
         public string VergiDairesi{ get; set; }
-        public DateTime Saat{ get; set; }
+
+        [Column(TypeName = "Char")]
+        [StringLength(5)]
+        public string Saat{ get; set; }
 
         [Column(TypeName = "Varchar")]
         [StringLength(30)]
@@ -32,6 +35,8 @@ namespace OnlineTicariOtomasyon.Models.Sınıflar
         [Column(TypeName = "Varchar")]
         [StringLength(30)]
         public string TeslimAlan{ get; set; }
+
+        public decimal Toplam { get; set; }
         public ICollection<FaturaKalem> FaturaKalems{ get; set; }
     }
 }
